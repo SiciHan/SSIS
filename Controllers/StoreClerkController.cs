eed0598f06088fba028eb9ffa96c4f530da1b1ef
jamescheang <cheang.james@gmail.com>
2020-01-27 22:46:59 +0800

    Figuring out how to redistribute items from other disbursements

--- Controllers/StoreClerkController.cs
+++ Controllers/StoreClerkController.cs
@@ -25,7 +25,7 @@ namespace Team8ADProjectSSIS.Controllers
             return View();
         }
 
-        // James
+        // James: Disbursement overview
         public ActionResult Disbursement()
         {
             // retrieve 2 lists of Disbursement Lists which are "Prepared" and "Scheduled" under the same Coll Point
@@ -39,7 +39,7 @@ namespace Team8ADProjectSSIS.Controllers
             return View();
         }
 
-        // James
+        // James: Selecting multiple disbursements to schedule for collection
         [HttpPost]
         public ActionResult Schedule(IEnumerable<int> disbIdsToSchedule)
         {
@@ -47,7 +47,30 @@ namespace Team8ADProjectSSIS.Controllers
             return RedirectToAction("Disbursement");
         }
 
-        // James
+        // James: Scheduling a single disbursement with redistribution if necessary
+        [HttpPost]
+        public ActionResult ScheduleSingle(IEnumerable<int> disbId, IList<int> itemId, IList<int> transferQtyNum)
+        {
+            //Debugging and PoC
+            System.Diagnostics.Debug.WriteLine(disbId);
+            disbId.ToList().ForEach(x => System.Diagnostics.Debug.WriteLine(x));
+            System.Diagnostics.Debug.WriteLine($"itemId Count: {itemId.Count}, transferQtyNum Count: {transferQtyNum.Count}");
+            foreach (int i in itemId)
+                System.Diagnostics.Debug.WriteLine("itemId: " + i);
+
+            foreach (int i in transferQtyNum)
+                System.Diagnostics.Debug.WriteLine("transferQtyNum: " + i);
+
+            for (int i = 0; i < itemId.Count; i++)
+            {
+                System.Diagnostics.Debug.WriteLine("itemId: " + itemId[i]);
+            }
+
+            _disbursementDAO.UpdateStatus(disbId);
+            return RedirectToAction("Disbursement");
+        }
+
+        // James: Opens page to redistribute qty from other disbursements
         public ActionResult Redistribute(int disbId)
         {
             //var disbId = Url.RequestContext.RouteData.Values["id"];
