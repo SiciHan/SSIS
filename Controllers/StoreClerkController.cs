e0f8019d0c787ad4a22b09e95e25df132ebacf09
James Cheang <cheang.james@gmail.com>
2020-01-27 15:57:08 +0800

    Disbursement and redistribution page

--- Controllers/StoreClerkController.cs
+++ Controllers/StoreClerkController.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Team8ADProjectSSIS.DAO;
+using Team8ADProjectSSIS.Models;
 
 namespace Team8ADProjectSSIS.Controllers
 {
     public class StoreClerkController : Controller
     {
+        private readonly DisbursementDAO _disbursementDAO;
+        private readonly DisbursementItemDAO _disbursementItemDAO;
+
+        public StoreClerkController()
+        {
+            _disbursementDAO = new DisbursementDAO();
+            _disbursementItemDAO = new DisbursementItemDAO();
+        }
+
         // GET: StoreClerk
         public ActionResult Index()
         {
             return View();
+        }
+
+        // James
+        public ActionResult Disbursement()
+        {
+            // retrieve 2 lists of Disbursement Lists which are "Prepared" and "Scheduled" under the same Coll Point
+            // find by Status and Collection Point
+            List<Disbursement> prepList= _disbursementDAO.FindByStatus("Prepared");
+            List<Disbursement> scheList= _disbursementDAO.FindByStatus("Scheduled");
+
+            ViewBag.prepList = prepList;
+            ViewBag.scheList = scheList;
+
+            return View();
+        }
+
+        // James
+        [HttpPost]
+        public ActionResult Schedule(IEnumerable<int> disbIdsToSchedule)
+        {
+            _disbursementDAO.UpdateStatus(disbIdsToSchedule);
+            return RedirectToAction("Disbursement");
+        }
+
+        // James
+        public ActionResult Redistribute(int disbId)
+        {
+            //var disbId = Url.RequestContext.RouteData.Values["id"];
+            Disbursement targetDisbursement = _disbursementDAO.FindById(disbId);
+            ViewBag.disb = targetDisbursement;
+
+            return PartialView("Redistribute", targetDisbursement);
         }
     }
 }
--- DAO/DisbursementDAO.cs
+++ DAO/DisbursementDAO.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using Team8ADProjectSSIS.Models;
 
 namespace Team8ADProjectSSIS.DAO
 {
     public class DisbursementDAO
     {
+        private readonly SSISContext context;
+
+        public DisbursementDAO()
+        {
+            this.context = new SSISContext();
+        }
+
+        //James
+        public List<Disbursement> FindByStatus(String status)
+        {
+            var list = context.Disbursements.Where(x => x.Status.Label == status)
+                .ToList();
+
+            return list;
+        }
+
+        //James
+        public void UpdateStatus(IEnumerable<int> disbIdsToSchedule)
+        {
+            try
+            {
+                context.Disbursements.Where(x => disbIdsToSchedule.Contains(x.IdDisbursement))
+                    .ToList()
+                    .ForEach(x => x.IdStatus = 10);
+                context.SaveChanges();
+            } catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+            }
+        }
+
+        public Disbursement FindById(int disbId)
+        {
+            return context.Disbursements.Where(x => x.IdDisbursement == disbId).FirstOrDefault();
+        }
     }
 }
