eada6474f1adf9503e538d6ce8d6ad3f30f082e2
SiciHan <32950564+SiciHan@users.noreply.github.com>
2020-01-30 14:05:53 +0800

    restrict the length of hashpassword. Added the Log in page and client side validation Added the AuthenticateFilter and AuthorizeFilter

---
+++ Filters/AuthenticateFilter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using System.Web.Routing;
+
+namespace Team8ADProjectSSIS.Filters
+{
+    public class AuthenticateFilter : ActionFilterAttribute, IAuthenticationFilter
+
+    {
+        private bool IsAuthOK = false;
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            //if the session does not expire yet
+            var sessionId = HttpContext.Current.Session["sessionId"];
+            if (sessionId != null && sessionId is Guid)
+            {
+                IsAuthOK = true;
+            }
+            //If authentication failed
+            else {
+                //session expired
+                if (sessionId == null) {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary{
+                        { "controller", "Home" },
+                        { "action", "SessionExpired" } }
+                      );
+                }
+                //illegal user
+                if(!(sessionId is Guid))
+                {
+                    filterContext.Result = new HttpUnauthorizedResult();
+                }
+            }
+        }
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (!IsAuthOK)
+            {
+                HttpContext.Current.Session.Clear();
+            }
+            
+        }
+    }
+}
---
+++ Migrations/202001230924016_snapshot11.cs
@@ -0,0 +1,20 @@
+﻿namespace Team8ADProjectSSIS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class snapshot11 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Employees", "UserName", c => c.String(nullable: false));
+            AlterColumn("dbo.Employees", "HashedPassward", c => c.String(nullable: false, maxLength: 32));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Employees", "HashedPassward", c => c.String());
+            AlterColumn("dbo.Employees", "UserName", c => c.String());
+        }
+    }
+}
