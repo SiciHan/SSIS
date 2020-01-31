cc9d31bee1ee158aef37783a7338bc6ca019a43a
SiciHan <32950564+SiciHan@users.noreply.github.com>
2020-01-31 12:00:06 +0800

    PurchaseOrderDAO and StoreClerkController

--- Controllers/StoreClerkController.cs
+++ Controllers/StoreClerkController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Team8ADProjectSSIS.DAO;
+using Team8ADProjectSSIS.Models;
 
 namespace Team8ADProjectSSIS.Controllers
 {
     public class StoreClerkController : Controller
     {
+        private readonly PurchaseOrderDAO purchaseOrderDAO;
+
         // GET: StoreClerk
         public ActionResult Index()
         {
@@ -15,7 +19,13 @@ namespace Team8ADProjectSSIS.Controllers
         }
         public ActionResult PurchaseOrderList()
         {
-
+            ViewData["Incomplete"]=purchaseOrderDAO.FindIncompletePO();
+            ViewData["Pending"]=purchaseOrderDAO.FindPendingPO();
+            ViewData["Rejected"]=purchaseOrderDAO.FindRejectedPO();
+            ViewData["Approved"]=purchaseOrderDAO.FindApprovedPO();
+            ViewData["Delivered"]=purchaseOrderDAO.FindDeliveredPO();
+            ViewData["Cancelled"]=purchaseOrderDAO.FindCancelledPO();
+            ViewData["IsStockLow"]=
             return View();
         }
     }
