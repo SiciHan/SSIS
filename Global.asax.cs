e3bcfaef4562c9eb7914a9b2fe7b0cf1b7cbc574
JustinYu2019 <e0003064@u.nus.edu>
2020-02-04 15:20:01 +0800

    Added WebApiConfig files installed web api packages added GlobalConfig in application Start

--- Global.asax.cs
+++ Global.asax.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Team8ADProjectSSIS.App_Start;
 using Team8ADProjectSSIS.DAO;
 
 namespace Team8ADProjectSSIS
@@ -13,6 +15,7 @@ namespace Team8ADProjectSSIS
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
