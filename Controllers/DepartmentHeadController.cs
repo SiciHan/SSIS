ac2c69bbc604ad0a530f6635b59229260311a877
JustinYu2019 <e0003064@u.nus.edu>
2020-02-06 00:47:14 +0800

    Added View Stationery req lists, and click to approve and reject, however there is one logic not right.

--- Controllers/DepartmentHeadController.cs
+++ Controllers/DepartmentHeadController.cs
@@ -3,14 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Team8ADProjectSSIS.DAO;
+using Team8ADProjectSSIS.Models;
 
 namespace Team8ADProjectSSIS.Controllers
 {
     public class DepartmentHeadController : Controller
     {
-        // GET: DepartmentHead
-        public ActionResult Index()
+        EmployeeDAO _employeeDAO;
+        RequisitionDAO _requisitionDAO;
+        RequisitionItemDAO _requisitionItemDAO;
+        ItemDAO _itemDAO;
+        public DepartmentHeadController()
         {
+            _employeeDAO = new EmployeeDAO();
+            _requisitionDAO= new RequisitionDAO();
+            _requisitionItemDAO = new RequisitionItemDAO();
+            _itemDAO = new ItemDAO();
+        }
+
+
+        // show lists of requisitions
+        public ActionResult PendingLists()
+        {
+            String codeDepartment = "ENGL"; // temporary use this employee id 36 with engL
+            
+            List<Requisition> empReqList=_employeeDAO.RaisesRequisitions(codeDepartment);
+            ViewBag.empReqList = empReqList;
+            return View();
+        }
+
+        // show the pending detail
+        public ActionResult PendingDetail(int idRequisition)
+        {
+            // need to find Requisition by this idReq by employee
+            Requisition requisition =_requisitionDAO.FindRequisitionByRequisionId(idRequisition);
+
+            // find the RequisitionItems to get unit requests by this idReq
+            
+            List<RequisitionItem> requisitionItemList = _requisitionItemDAO.FindRequisitionItem(idRequisition);
+            
+            
+            ViewData["requisition"] = requisition;
+            ViewData["requisitionItemList"] = requisitionItemList;
+
+            
+            return View();
+        }
+        
+        public ActionResult Approve(int idRequisition)
+        {
+            _requisitionDAO.UpdateApproveStatus(idRequisition);
+            
+            return RedirectToAction("PendingLists", "DepartmentHead");
+        }
+        public ActionResult Reject(int idRequisition)
+        {
+            _requisitionDAO.UpdateRejectStatus(idRequisition);
+            return RedirectToAction("PendingLists", "DepartmentHead");
+        }
+
+        // view CurrentRep and CP
+        public ActionResult CurrentRepCP()
+        {
+            return View();
+        }
+
+        // change Rep and CP
+        public ActionResult ChangeRepCP()
+        {
+            return View();
+        }
+        public ActionResult Delegation()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            // need to redirect to Login page
             return View();
         }
     }
--- DAO/EmployeeDAO.cs
+++ DAO/EmployeeDAO.cs
@@ -14,7 +14,29 @@ namespace Team8ADProjectSSIS.DAO
         {
             this.context = new SSISContext();
         }
-
+        // SH
+        public List<Employee> FindEmployeeListByDepartment(string codeDepartment)
+        {
+            return context.Employees.OfType<Employee>().Where(x => x.CodeDepartment.Equals(codeDepartment)).ToList();
+        }
+        //SH
+        public List<Requisition> RaisesRequisitions(string codeDepartment)
+        {
+            List<Employee> empList = FindEmployeeListByDepartment(codeDepartment);
+            List<Requisition> reqList = context.Requisitions.ToList(); // to find the list of requisitions
+            List<Requisition> empReqList = new List<Requisition>(); // find req list of employee belong to specific department 
+            foreach(Employee e in empList)
+            {
+                foreach(Requisition r in reqList)
+                {
+                    if (r.IdEmployee==e.IdEmployee)
+                    {
+                        empReqList.Add(r);
+                    }
+                }
+            }
+            return empReqList;
+        }
         public Employee FindEmployeeByUsername(string username)
         {
             return context.Employees.OfType<Employee>().Where(x => x.UserName.Equals(username)).FirstOrDefault();
